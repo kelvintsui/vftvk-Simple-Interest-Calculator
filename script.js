0038e9633bc9be083481df7d7be25ff75245d5ab
kelvintsui <tsuichunhing@gmail.com>
2022-03-15 23:33:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var intPrincipal = parseInt(principal);
-var floatRate = parseFloat(rate);
-var intYears = parseInt(years);
+(function(){
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var intPrincipal = parseInt(principal);
+    var floatRate = parseFloat(rate);
+    var intYears = parseInt(years);
 
-function compute() {
-    var simpleInterest = (intPrincipal * floatRate * intYears) / 100;
-    var targetYear = new Date().getFullYear() + intYears;
-    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + intPrincipal + "</mark>,<br/>" +
-        "at an interest rate of <mark>" + floatRate + "</mark>,<br/>" +
-        "You will receive an amount of <mark>" + simpleInterest + "</mark>,<br/> " +
-        "in the year <mark>" + targetYear + "</mark>";
+    function compute() {
+        var simpleInterest = (intPrincipal * floatRate * intYears) / 100;
+        var targetYear = new Date().getFullYear() + intYears;
+        document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + intPrincipal + "</mark>,<br/>" +
+            "at an interest rate of <mark>" + floatRate + "</mark>,<br/>" +
+            "You will receive an amount of <mark>" + simpleInterest + "</mark>,<br/> " +
+            "in the year <mark>" + targetYear + "</mark>";
 
-}
-
-function getSliderValue() {
-    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
-}
-
-function validateAmount() {
-    if (intPrincipal < 1) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
     }
-}
+
+    function getSliderValue() {
+        document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+    }
+
+    function validateAmount() {
+        if (intPrincipal < 1) {
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+        }
+    }
+});
