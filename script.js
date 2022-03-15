5ee019df955ca84894088e03a45c8668c446585d
kelvintsui <tsuichunhing@gmail.com>
2022-03-16 00:31:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var rateSlider = document.getElementById("rateSlider");
+document.getElementById("rateSliderPreview").innerHTML = rateSlider.value + "%";
+
+rateSlider.oninput = function () {
+  document.getElementById("rateSliderPreview").innerHTML = this.value + "%";
 }
-        
+
+function compute() {
+  var principal = parseInt(document.getElementById("principal").value);
+  if (principal < 1 || isNaN(principal)) {
+    alert("Enter Amount!");
+    document.getElementById("principal").focus();
+  } else {
+    var rate = parseFloat(document.getElementById("rateSlider").value);
+    var years = parseInt(document.getElementById("years").value);
+    var simpleInterest = (principal * rate * years) / 100;
+    var targetYear = new Date().getFullYear() + years;
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+      "at an interest rate of <mark>" + rate + "%</mark>,<br/>" +
+      "You will receive an amount of <mark>" + simpleInterest + "</mark>,<br/> " +
+      "in the year <mark>" + targetYear + "</mark>";
+  }
+}
